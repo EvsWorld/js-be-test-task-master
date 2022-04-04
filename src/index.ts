5fce08bfcc1766521f71e47ab4149896a70888d7
evsworld <evanlhendrix@gmail.com>
2022-04-04 20:32:05 +0200

    fetching media and media context

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 import express from "express";
+import cors from "cors";
+import axios from "axios";
 import "./externalService";
 
 const app = express();
 const port = 3000;
 
 app.get("/api/sessions/:sessionId", async (req, res) => {
+  // console.log("req :>> ", req);
+  const { sessionId } = req.params;
+  console.log("sessionId :>> ", sessionId);
   try {
-    return res.status(501).json();
+    // call the other end points to collect and filter the required data
+    // call media and get {id, memeType, context}
+    const media = await axios.get(
+      `https://api.company .internal/sessions/${sessionId}/media`
+    );
+    console.log("media :>> ", media.data);
+    const mediaContext = await axios.get(
+      `https://api.company .internal/media-context/${sessionId}`
+    );
+    console.log("mediaContext :>> ", mediaContext.data);
+    return res.status(200).json({ hello: "its me" });
   } catch (error: any) {
     console.error("Error happened", error);
     return res
